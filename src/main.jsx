426cd7fa619b07286847c3132f46638e41b39bd3
Ani Mukherjee <animukherjee2006@gmail.com>
2026-05-29 09:47:49 +0530

    fix: removed router basename for render deployment

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,9 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-
-    <StrictMode>
-    <BrowserRouter basename="/My-portfolio/">
-    
+  <StrictMode>
+    {/* basename ko hata diya hai taaki Render par sahi se chale */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
